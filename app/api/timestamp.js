865f6a61aa30ac4e43e1bd7fc80b4f65f78b935a
Rafael J. Rodriguez <rafase282@gmail.com>
2016-01-01 03:53:34 +0000

    Netural not returning null

--- app/api/timestamp.js
+++ app/api/timestamp.js
@@ -4,8 +4,23 @@ module.exports = function(app) {
 
     app.get('/:query', function(req, res) {
         var date = req.params.query;
-        var unix = Math.round(new Date(+date).getTime()/1000);
-        var dateObj = { "unix": unix, "natural": date };
+        var unix = getUnixTime(date);
+        var natural = checkNaturalTime(date, unix);
+        var dateObj = { "unix": unix, "natural": natural };
         res.send(dateObj);
     });
+    
+    function getUnixTime(date) {
+        // Return the Unix time
+        return Math.round(new Date(+date).getTime()/1000);
+    }
+    
+    function checkNaturalTime(date, unix) {
+        // Deal with Natural Time
+        if (date == null) {
+            return unix;
+        } else {
+            return date;
+        }
+    }
 };
