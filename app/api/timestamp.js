de797911e5f5beb2a911f7d0224e317fea4218fc
Rafael J. Rodriguez <rafase282@gmail.com>
2016-01-01 03:36:23 +0000

    Added proper unix time

--- app/api/timestamp.js
+++ app/api/timestamp.js
@@ -3,7 +3,9 @@
 module.exports = function(app) {
 
     app.get('/:query', function(req, res) {
-        var search = req.params.query;
-        res.send(search);
+        var date = req.params.query;
+        var unix = Math.round(new Date(+date).getTime()/1000);
+        var dateObj = { "unix": unix, "natural": date };
+        res.send(dateObj);
     });
 };
