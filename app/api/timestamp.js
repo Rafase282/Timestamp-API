d0bac2d8c4b1abdcd2bb850137373133f93ad082
Rafael J. Rodriguez <rafase282@gmail.com>
2016-01-01 07:18:56 +0000

    Almost done

--- app/api/timestamp.js
+++ app/api/timestamp.js
@@ -1,26 +1,32 @@
 'use strict';
+var moment = require('moment');
 
 module.exports = function(app) {
 
     app.get('/:query', function(req, res) {
         var date = req.params.query;
-        var unix = getUnixTime(date);
-        var natural = checkNaturalTime(date, unix);
-        var dateObj = { "unix": unix, "natural": natural };
-        res.send(dateObj);
+        if (validateDate(date)) {
+            var unix = natToUnix(date);
+            var natural = unixToNat(unix); 
+            var dateObj = { "unix": unix, "natural": natural };
+            res.send(JSON.stringify(dateObj));
+        }
+        
     });
     
-    function getUnixTime(date) {
-        // Return the Unix time
-        return Math.round(new Date(+date).getTime()/1000);
+    function natToUnix(date) {
+        // Conver from natural date to unix timestamp
+        return moment(date).format("X");
     }
     
-    function checkNaturalTime(date, unix) {
-        // Deal with Natural Time
-        if (date == null) {
-            return unix;
-        } else {
-            return date;
-        }
+    function unixToNat(unix) {
+        // Convert unix timestamp to natural date
+        return moment.unix(unix).format("MMMM D, YYYY");
+    }
+    
+    function validateDate(date) {
+        // Validate that the string is an actual date, either natural or unix
+        
+        return Math.round(new Date(+date).getTime()/1000);
     }
 };
